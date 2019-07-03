8cca28e74c06c66cba14574e1038544be2ccc200
bibhuti <bibhuti.nak@gmail.com>
2019-07-03 16:59:04 -0700

    Added owner name on bikeshare.py greeting section

--- bikeshare.py
+++ bikeshare.py
@@ -18,7 +18,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello!I am Bibhuti! Let\'s explore some US bikeshare data!')
     #city = ''
     #city = str(input("Please enter name of city you like to analyze, select from 'Chicago, New York City, Washington: "))
     #city = city.lower()
